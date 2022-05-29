e19b06ea12ee8cbebb82c95ef95b47f3bc7b2cdf
Jordan <JBuren1957@Gmail.com>
2022-05-28 20:07:20 -0500

    First Draft

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript"
+h2.textContent = "The Journey of Learning to Code"
 document.querySelector("body").appendChild(h2);
