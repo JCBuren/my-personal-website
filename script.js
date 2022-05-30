0bb417f67b46f171f41b75bbc366a52868d88b65
JCBuren <105258972+JCBuren@users.noreply.github.com>
2022-05-30 13:22:23 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
-const h2 = document.createElement("h2");
-h2.textContent = "The Journey of Learning to Code"
-document.querySelector("body").appendChild(h2);
+const comingSoonMessage = "More Content Coming Soon!!!";
+
+document.querySelector(".projects").addEventListener('click', function() {
+  alert(comingSoonMessage)
+});
+
+document.querySelector(".ideas").addEventListener('click', function() {
+  alert(comingSoonMessage)
+});
+
+document.getElementById("resume").addEventListener('click', function() {
+  alert(comingSoonMessage)
+});
+
+document.getElementById("linkedin").addEventListener('click', function() {
+  alert(comingSoonMessage)
+});
